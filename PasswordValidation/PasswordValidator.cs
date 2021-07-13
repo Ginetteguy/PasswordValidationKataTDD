cb52ff84a4d610c58f45cf14edf1abd6093fa649
Ginetteguy <godin.axel@laposte.net>
2021-07-13 14:35:47 +0200

    When password contains less than 2 numbers, should return error

--- PasswordValidation/PasswordValidator.cs
+++ PasswordValidation/PasswordValidator.cs
@@ -9,6 +9,12 @@ namespace PasswordValidation
             if (password.Length < 8)
                 return "Password must be at least 8 characters";
 
+            int numberCounter = 0;
+            Array.ForEach(password.ToCharArray(), delegate (char character) { if (Char.IsNumber(character)) { numberCounter++; } });
+
+            if (numberCounter < 2)
+                return "The password must contain at least 2 numbers";
+
             return string.Empty;
         }
     }
--- PasswordValidationTests/PasswordValidatorTests.cs
+++ PasswordValidationTests/PasswordValidatorTests.cs
@@ -17,5 +17,18 @@ namespace PasswordValidationTests
             // Assert
             Assert.AreEqual("Password must be at least 8 characters", result);
         }
+
+        [Test]
+        public void WhenPasswordDoesntContains2NumbersAtLeast_ShouldReturnError()
+        {
+            // Arrange
+            const string password = "ABCDEFGH";
+
+            // Setup
+            string result = PasswordValidator.Validate(password);
+
+            // Assert
+            Assert.AreEqual("The password must contain at least 2 numbers", result);
+        }
     }
 }
